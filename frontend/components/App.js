91c903eb213b8a785217b62b78ffc0a1e86bdd86
renee.troiani@gmail.com <renee.troiani@gmail.com>
2022-04-11 20:32:55 -0600

    can type in input box to add new todo

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Form from './Form';
 
 export default class App extends React.Component {
+
+
   render() {
     return (
       <div>
         Todo App
+        <Form onSubmit={Function.prototype}/>
       </div>
     )
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,28 @@
 import React from 'react'
 
+const initialState = {
+  todoText: ''
+}
+
 export default class Form extends React.Component {
+  state = initialState
+
+  handleChanges = evt => {
+    this.setState({
+      todoText: evt.target.value
+    })
+  }
   render() {
     return (
-      <div>
-        Form
-      </div>
+      <form>
+        <input 
+        type='text'
+        name='todo'
+        value={this.state.todoText}
+        onChange={this.handleChanges}
+        />
+        <button>Add Todo</button>
+      </form>
     )
   }
 }
